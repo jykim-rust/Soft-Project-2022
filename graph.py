f1259b57b12aa9813f55e69deff9679c9f591a94
jykim-rust <alexj99@naver.com>
2022-06-05 23:55:58 +0900

    [JYK] #issue_id

--- graph.py
+++ graph.py
@@ -1,6 +1,9 @@
 from cgi import parse_qs
+import matplotlib as mpl
+mpl.use('Agg')
 from template import html
 import matplotlib.pyplot as plt
+
 
 def application(environ, start_response):
     if environ['PATH_INFO'] == './img/graph.png':
